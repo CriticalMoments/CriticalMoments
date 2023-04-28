9a299a877f2cb5291de8d58b3d3e4c76123b09d1
scosman <scosman@users.noreply.github.com>
2023-04-28 12:35:20 -0400

    Add docs, and a doc build tool.
    To generate documentation:
     - Document using apple standards: https://nshipster.com/objective-c-documentation/
     - Install and run `jazzy` (https://github.com/realm/jazzy)

--- ios/Sources/CriticalMoments/messaging/CMBannerManager.h
+++ ios/Sources/CriticalMoments/messaging/CMBannerManager.h
@@ -16,14 +16,45 @@ typedef NS_ENUM(NSUInteger, CMAppWideBannerPosition) {
     CMAppWideBannerPositionTop
 };
 
+/**
+ Manages the presentation of banner messages across your app.
+ 
+ Example usage:
+ ```
+ CMBannerMessage* bannerMessage = [[CMBannerMessage alloc] initWithBody:@"Important info..."];
+ [CMBannerManager.sharedInstance showAppWideMessage:bannerMessage];
+ ```
+ */
 @interface CMBannerManager : NSObject
 
+/**
+ Set this to configure if the banner messages are presented at the top or bottom of your app.
+ @warning Be sure to test your app renders well with the chosen position. The banner manager will resize your root view controller to make room for the banner; if you've hard coded offsets, for example the notch or dyamic island, then your app layout may be a bit strange. If you encounter issues, you should adapt use apple layout guides for any offsets, which will solve most of these issues and help on future hardware.
+ @see CMAppWideBannerPosition enum
+ */
 @property (nonatomic) CMAppWideBannerPosition appWideBannerPosition;
 
+/**
+ A shared instance reference. You should only use a single instance of CMBannerManager per app. This sharedInstance is suggested for convenience, but you can also create and maintain your own instance if you prefer.
+ @return a shared instance of CMBannerManager
+ */
 +(CMBannerManager*) sharedInstance;
 
+/**
+ Shows a banner across your entire application, shifting the root viewcontroller of your key window. If called multiple times, will include UI to iterate though multiple banners.
+ @param message the CMBannerMessage to present
+ */
 -(void) showAppWideMessage:(CMBannerMessage*)message;
+
+/**
+Removes a previously presented banner message.
+ @param message the CMBannerMessage to remove
+ */
 -(void) removeAppWideMessage:(CMBannerMessage*)message;
+
+/**
+ Removes all  previously presented banner messages.
+ */
 -(void) removeAllAppWideMessages;
 
 @end
--- ios/Sources/CriticalMoments/messaging/CMBannerMessage.h
+++ ios/Sources/CriticalMoments/messaging/CMBannerMessage.h
@@ -13,30 +13,71 @@ NS_ASSUME_NONNULL_BEGIN
 
 @class CMBannerMessage;
 
+/**
+ A protocol for the action to call when a banner message is tapped.
+ @param message the CMBannerMessage which was tapped
+ @see CMBannerMessage.actionDelegate
+ */
 @protocol CMBannerActionDelegate
 -(void) messageAction:(CMBannerMessage*)message;
 @end
 
-@protocol CMBannerDismissDelegate
--(void) dismissedMessage:(CMBannerMessage*)message;
-@end
-
-@protocol CMBannerNextMessageDelegate
--(void) nextMessage;
-@end
-
+/**
+ This class represents individual banner message which can be presented across the top or bottom of your app.
+ 
+ It will take the default theme, which controls the font and colors.
+ 
+ This class can be subclassed to implement completely custom views. Before subclassing, try using themes to achieve desired look. If you choose to subclass:
+  - You should implement buildViewForMessage
+  - Your view should have dismiss buttons, which targets the dismissTapped method
+  - Your view should have a "next message" button, which targets the nextMessageButtonTapped method
+  - Your view should call the actionDelegate on tap
+  - Text should be loaded from the body property. If you hardcode, server driven messaging won't be available
+  - You should respect showDismissButton and maxLineCount properties in your view
+ */
 @interface CMBannerMessage : NSObject
 
+/**
+ The body text to be rendered in the banner
+ */
 @property (nonatomic, readonly) NSString* body;
+
+/**
+ Controls if a dismiss button ("X") is rendered, allowing user to close the banner. Defaults to YES.
+ */
 @property (nonatomic, readwrite) bool showDismissButton;
+
+/**
+ Controlls the max number of lines allowed in the banner message text. Defaults to 4. Set to 0 for unlimited, although banners are still capped at 20% of screen height and will eventually tuncate.
+ */
 @property (nonatomic, readwrite) NSNumber* maxLineCount;
+
+/**
+ This action delegate will be called when the banner message is tapped. If not set, tapping will not perform any action.
+ */
 @property (nonatomic, readwrite) id<CMBannerActionDelegate> actionDelegate;
 
 -(instancetype)init NS_UNAVAILABLE;
 
+/**
+ @param body The body text to be rendered in the banner
+ */
 -(instancetype)initWithBody:(NSString*)body;
 
+/**
+ This method is only exposed for subclassing and should not be called in normal usage. See CMBannerMessage class documentation for notes on how to subclass properly.
+ */
 -(UIView*) buildViewForMessage;
+
+/**
+ This method is only exposed for subclassing and should not be called in normal usage. See CMBannerMessage class documentation for notes on how to subclass properly.
+ */
+- (void)dismissTapped:(UIButton*)sender;
+
+/**
+ This method is only exposed for subclassing and should not be called in normal usage. See CMBannerMessage class documentation for notes on how to subclass properly.
+ */
+- (void)nextMessageButtonTapped:(UIButton*)sender;
 
 @end
 
