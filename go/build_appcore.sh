f27bbbae1776012274eb2b347e368107f0876400
scosman <scosman@users.noreply.github.com>
2023-05-02 17:03:21 -0400

    move the go checksum files into the go folder.

--- go/build_appcore.sh
+++ go/build_appcore.sh
@@ -7,8 +7,8 @@ cd $dir
 # no op if the go folder hasn't changed. This verifies the code and 
 # xcframework build match. 
 # Saves time when developing, and ensures edits are built before running.
-find . -type f ! -iname ".DS_Store" -exec md5 {} \; > ../.go_folder_hashlist
-diff ../.go_folder_hashlist ../.go_folder_last_build_hashlist > /dev/null 2>&1
+find . -type f ! -iname ".DS_Store" ! -iname ".go_*_hashlist" -exec md5 {} \; > ./.go_folder_hashlist
+diff ./.go_folder_hashlist ./.go_folder_last_build_hashlist > /dev/null 2>&1
 folderChanged=$?
 if [ $folderChanged -eq 0 ]
 then
@@ -18,8 +18,8 @@ fi
 
 echo "Building golang appcore framework..."
 # remove prior build and build hashlist
-rm -r ./appcore/build/Appcore.xcframework
-rm ../.go_folder_last_build_hashlist
+rm -r ./appcore/build/Appcore.xcframework > /dev/null 2>&1
+rm ../.go_folder_last_build_hashlist > /dev/null 2>&1
 
 gomobile bind -target ios,iossimulator -iosversion=11 -ldflags '-w -s' -o appcore/build/Appcore.xcframework ./appcore
 buildSuccess=$?
@@ -27,7 +27,7 @@ buildSuccess=$?
 if [ $buildSuccess -eq 0 ]
 then
     echo "Build succeeded!"
-    find . -type f ! -iname ".DS_Store" -exec md5 {} \; > ../.go_folder_last_build_hashlist
+    find . -type f ! -iname ".DS_Store" ! -iname ".go_*_hashlist" -exec md5 {} \; > ./.go_folder_last_build_hashlist
     exit 0
 else
     echo "Build failed. Either fix compiler issues, or re-sync the go folder from remote git repo."
