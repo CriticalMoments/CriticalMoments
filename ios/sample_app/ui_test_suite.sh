6c5799826e959199c09bd5dd7d1881ee96f31bb7
scosman <scosman@users.noreply.github.com>
2023-10-03 02:31:00 -0400

    Update test suite script

--- ios/sample_app/ui_test_suite.sh
+++ ios/sample_app/ui_test_suite.sh
@@ -9,14 +9,14 @@ xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS S
 # iPad Pro 12.9, 16.4
 xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=16.4,name=iPad Pro (12.9-inch) (6th generation)' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test
 
+# iPhone 11, 13.7
+xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=13.7,name=iPhone 11' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test
+
 # iPhone 6s, 15.5
+# Erroring, but in signing. P2
 xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 6s' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test
 
-# iPhone 11, 13.7
-# xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=13.7,name=iPhone 11' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test
-
-# TODO: horizontal iPad, iOS 12 on HW, iPhone 14 on HW.
-echo "Simulator not working for iOS 13, see script"
-echo "Manual tests required for horizontal iPad (rotate simulator before running), iOS 12 (old HW), and iPhone 14 pro HW"
+echo "Manually: Run this suite on iPhone 14 pro HW as well"
+echo "Manually: Run manual test on iOS 12 (old iPhone 6 HW) -- snapshot test library does not compile to ios 12"
 
 
