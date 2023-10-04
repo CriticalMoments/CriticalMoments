9243e6f543edc650270c2478bd625539d213e801
scosman <scosman@users.noreply.github.com>
2023-10-04 03:47:32 -0400

    UI Testing Script finalized. All iOS versions 13+, and a range of iPhones and iPads.

--- ios/sample_app/ui_test_suite.sh
+++ ios/sample_app/ui_test_suite.sh
@@ -1,21 +1,76 @@
 #!/bin/sh
 
-# Exit on first error
-set -e
+spinner()
+{
+    local pid=$!
+
+    spin='-\|/'
+
+    i=0
+    while kill -0 $pid 2>/dev/null
+    do
+      i=$(( (i+1) %4 ))
+      printf "\r${spin:$i:1}"
+      sleep .1
+    done
+
+    printf "\b"
+}
 
 # iPhone 14 Pro, 16.4
-xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=16.4,name=iPhone 14 Pro' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test
+echo "Running UI Tests For iPhone 14 Pro, on iOS 16.4"
+xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=16.4,name=iPhone 14 Pro' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test &> /dev/null & spinner
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "Passed\n\n"
+else
+  echo "Failed\n"
+  exit 99
+fi
 
-# iPad Pro 12.9, 16.4
-xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=16.4,name=iPad Pro (12.9-inch) (6th generation)' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test
+echo "Running UI tests on iPad Pro 12.9, ios 16.4"
+xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=16.4,name=iPad Pro (12.9-inch) (6th generation)' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test  &> /dev/null & spinner
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "Passed\n\n"
+else
+  echo "Failed\n"
+  exit 99
+fi
 
-# iPhone 11, 13.7
-xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=13.7,name=iPhone 11' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test
+echo "Running UI tests on iPhone 11, ios 13.7"
+xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=13.7,name=iPhone 11' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test  &> /dev/null & spinner
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "Passed\n\n"
+else
+  echo "Failed\n"
+  exit 99
+fi
 
-# iPhone 6s, 15.5
-xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 6s' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test
+echo "Running UI tests on iPhone 6s Plus, ios 15.5"
+# problems
+xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=15.5,name=iPhone 6s Plus' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test &> /dev/null & spinner
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "Passed\n\n"
+else
+  echo "Failed\n"
+  exit 99
+fi
 
-echo "Manually: Run this suite on iPhone 14 pro HW as well"
-echo "Manually: Run manual test on iOS 12 (old iPhone 6 HW) -- snapshot test library does not compile to ios 12"
+echo "Running UI tests on normal iPad (8th gen), ios 14.5"
+xcodebuild -scheme SampleApp -target SampleAppTests -destination 'platform=iOS Simulator,OS=14.5,name=iPad (8th generation)' '-only-testing:SampleAppTests/SnapshotTests/testScreenshotAllSampleAppFeatures' test &> /dev/null & spinner
+RESULT=$?
+if [ $RESULT -eq 0 ]; then
+  echo "Passed\n\n"
+else
+  echo "Failed\n"
+  exit 99
+fi
+
+echo "All Simulator Tests Passed! But you arn't done yet!"
+echo " - Manually: Run the snapshot test suite on iPhone 14 Pro HW using xcode"
+echo " - Manually: Run manual sanity check on iPhone 6 hardware, running iOS 12. No automation because snapshot test library does not compile to ios 12" 
 
 
