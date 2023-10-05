25acedef72b9649cf2a0b6691e9e6418710ca205
scosman <scosman@users.noreply.github.com>
2023-10-05 17:53:31 -0400

    Remove confusing CriticalMomentsSwift target. The wrapper is automagic, no need to write custom one.

--- Package.swift
+++ Package.swift
@@ -10,14 +10,11 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "CriticalMoments",
-            targets: ["CriticalMoments", "CriticalMomentsSwift"]),
+            targets: ["CriticalMoments"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
-        .target(
-            name: "CriticalMomentsSwift",
-            path: "ios/Sources/CriticalMomentsSwift"),
         .target(
             name: "CriticalMoments",
             dependencies: ["Appcore"],
@@ -26,10 +23,6 @@ let package = Package(
         .binaryTarget(
             name: "Appcore",
             path: "go/appcore/build/Appcore.xcframework"),
-        .testTarget(
-            name: "CriticalMomentsSwiftTests",
-            dependencies: ["CriticalMomentsSwift"],
-            path: "ios/Tests/CriticalMomentsSwiftTests"),
         .testTarget(
             name: "CriticalMomentsTests",
             dependencies: ["CriticalMoments"],
