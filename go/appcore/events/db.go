c97c48fc68b98d842d67e068f3d20abb80393f89
scosman <scosman@users.noreply.github.com>
2023-10-10 20:44:14 -0400

    Add database migration system. Simple but it works.

--- go/appcore/events/db.go
+++ go/appcore/events/db.go
@@ -26,12 +26,50 @@ func NewDB(dataDir string) (*DB, error) {
 		return nil, err
 	}
 
-	return &DB{
+	db := DB{
 		databasePath: dbPath,
 		sqldb:        sqldb,
-	}, nil
+	}
+
+	err = db.migrate()
+	if err != nil {
+		return nil, err
+	}
+
+	return &db, nil
 }
 
 func (db *DB) Close() error {
 	return db.sqldb.Close()
+}
+
+// migrations can be run on each start because they are incremental and non-destructive
+// Future migrations must also be incremental (only append to this, check if not exists)
+func (db *DB) migrate() error {
+	_, err := db.sqldb.Exec(`
+		CREATE TABLE IF NOT EXISTS events (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			event_name TEXT NOT NULL,
+			event_data TEXT NOT NULL,
+			created_at DATETIME,
+			updated_at DATETIME
+		);
+
+		CREATE TRIGGER IF NOT EXISTS insert_events_created_at 
+		AFTER INSERT ON events
+		BEGIN
+			UPDATE events SET created_at =unixepoch('subsec') WHERE id = NEW.id;
+		END;
+
+		CREATE TRIGGER IF NOT EXISTS update_events_updated_at 
+		AFTER UPDATE ON events
+		BEGIN
+			UPDATE events SET updated_at =unixepoch('subsec') WHERE id = NEW.id;
+		END;
+	`)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
--- go/appcore/events/db_test.go
+++ go/appcore/events/db_test.go
@@ -2,9 +2,12 @@ package events
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
 	"os"
+	"path"
 	"testing"
+	"time"
 )
 
 func testBuildTestDb(t *testing.T) *DB {
@@ -20,7 +23,7 @@ func testBuildTestDb(t *testing.T) *DB {
 	return db
 }
 
-func TestTestDB(t *testing.T) {
+func TestDBConnection(t *testing.T) {
 	db := testBuildTestDb(t)
 	defer db.Close()
 
@@ -38,5 +41,160 @@ func TestTestDB(t *testing.T) {
 
 	if v != 99 {
 		t.Fatal("DB test failed")
+	}
+}
+
+func TestDBMigrate(t *testing.T) {
+	db := testBuildTestDb(t)
+	defer db.Close()
+	// Test contructor runs migration
+	testSchema(db, t)
+
+	// run again to make sure it doesn't fail
+	err := db.migrate()
+	if err != nil {
+		t.Fatal(err)
+	}
+	testSchema(db, t)
+
+	// run cold from new DB instance (schema should already exist)
+	db.Close()
+	db2, err := NewDB(path.Dir(db.databasePath)[5:])
+	if err != nil {
+		t.Fatal(err)
+	}
+	testSchema(db2, t)
+}
+
+func testSchema(db *DB, t *testing.T) {
+	r, err := db.sqldb.Query("SELECT name FROM sqlite_schema WHERE type='table' AND name='events'")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Close()
+	r.Next()
+	var v string
+	err = r.Scan(&v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v != "events" {
+		t.Fatal("DB migation failed")
+	}
+
+	r, err = db.sqldb.Query("SELECT name FROM sqlite_schema WHERE type='trigger' AND name='insert_events_created_at'")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Close()
+	r.Next()
+	err = r.Scan(&v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v != "insert_events_created_at" {
+		t.Fatal("DB migration failed")
+	}
+
+	r, err = db.sqldb.Query("SELECT name FROM sqlite_schema WHERE type='trigger' AND name='update_events_updated_at'")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Close()
+	r.Next()
+	err = r.Scan(&v)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if v != "update_events_updated_at" {
+		t.Fatal("DB migration failed")
+	}
+}
+
+func BenchmarkWarmMigrate(b *testing.B) {
+	t := &testing.T{}
+	db := testBuildTestDb(t)
+	defer db.Close()
+
+	for i := 0; i < b.N; i++ {
+		err := db.migrate()
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
+func TestCreatedAtTrigger(t *testing.T) {
+	db := testBuildTestDb(t)
+	defer db.Close()
+
+	// insert a row into events
+	_, err := db.sqldb.Exec(`
+		INSERT INTO events (event_name, event_data)
+		VALUES ('test', 'test')
+	`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// select the last row inserted
+	r, err := db.sqldb.Query(`
+		SELECT created_at, updated_at FROM events
+		LIMIT 1
+	`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Close()
+	r.Next()
+	var c float64
+	var u float64
+	err = r.Scan(&c, &u)
+	if err != nil {
+		t.Fatal(err)
+	}
+	now := float64(time.Now().UnixNano()) / float64(time.Second)
+	if math.Abs(now-c) > 0.01 {
+		t.Fatal("Trigger failed to set created_at")
+	}
+	if math.Abs(now-u) > 0.01 {
+		t.Fatal("Trigger failed to set updated_at")
+	}
+
+	// update the row after small delay
+	time.Sleep(time.Millisecond * 2)
+	_, err = db.sqldb.Exec(`
+		UPDATE events SET event_name = 'test2'
+		WHERE event_name = 'test'
+	`)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// check created_at has not changed, but updated_at has
+	r, err = db.sqldb.Query(`
+		SELECT created_at, updated_at FROM events
+		LIMIT 1
+	`)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer r.Close()
+	r.Next()
+	var c2 float64
+	var u2 float64
+	err = r.Scan(&c2, &u2)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if c != c2 {
+		t.Fatal("created_at changed")
+	}
+	if u == u2 {
+		t.Fatal("Trigger did not change updated_at")
+	}
+	now = float64(time.Now().UnixNano()) / float64(time.Second)
+	if math.Abs(now-u2) > 0.01 {
+		t.Fatal("Trigger failed to set updated_at")
 	}
 }
