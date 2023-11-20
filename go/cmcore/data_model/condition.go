87d218a1627d9039d097f2e0c07d27cb820a7772
scosman <scosman@users.noreply.github.com>
2023-11-20 11:31:23 -0500

    Refactor built in property config
     - Make it a struct so we can add more properties to properties...
     - Add in a source property (library or client)
     - Merge well known into the one built in list. Much cleaner. Now these are source=client and optional=true
     - remove separate optional list

--- go/cmcore/data_model/condition.go
+++ go/cmcore/data_model/condition.go
@@ -47,96 +47,135 @@ func (c *Condition) String() string {
 
 const CMTimeKind = (reflect.Kind)(math.MaxUint)
 
-func BuiltInPropertyTypes() map[string]reflect.Kind {
-	return map[string]reflect.Kind{
-		"platform":                reflect.String,
-		"os_version":              reflect.String,
-		"device_manufacturer":     reflect.String,
-		"device_model":            reflect.String,
-		"device_model_class":      reflect.String,
-		"locale_language_code":    reflect.String,
-		"locale_country_code":     reflect.String,
-		"locale_currency_code":    reflect.String,
-		"app_version":             reflect.String,
-		"user_interface_idiom":    reflect.String,
-		"app_id":                  reflect.String,
-		"screen_width_pixels":     reflect.Int,
-		"screen_height_pixels":    reflect.Int,
-		"screen_width_points":     reflect.Int,
-		"screen_height_points":    reflect.Int,
-		"screen_scale":            reflect.Float64,
-		"device_battery_state":    reflect.String,
-		"device_battery_level":    reflect.Float64,
-		"device_low_power_mode":   reflect.Bool,
-		"device_orientation":      reflect.String,
-		"interface_orientation":   reflect.String,
-		"dark_mode":               reflect.Bool,
-		"network_connection_type": reflect.String,
-		"has_wifi_connection":     reflect.Bool,
-		"has_cell_connection":     reflect.Bool,
-		"has_active_network":      reflect.Bool,
-		"expensive_network":       reflect.Bool,
-		"cm_version":              reflect.String,
-		"foreground":              reflect.Bool,
-		"app_install_date":        CMTimeKind,
-		"timezone_gmt_offset":     reflect.Int,
-		"app_state":               reflect.String,
-		"has_watch":               reflect.Bool,
-		"screen_brightness":       reflect.Float64,
-		"screen_captured":         reflect.Bool,
+type CMPropertySampleType int
+
+const (
+	CMPropertySampleTypeAppStart    CMPropertySampleType = 1
+	CMPropertySampleTypeOnUse       CMPropertySampleType = 2
+	CMPropertySampleTypeOnCustomSet CMPropertySampleType = 3
+	CMPropertySampleTypeDoNotSample CMPropertySampleType = 4
+)
+
+type CMPropertySource int
+
+const (
+	// Lib properties are provided by CM library, and only CM library
+	CMPropertySourceLib CMPropertySource = iota
+	// Client properties are provided by the client, and only the client
+	CMPropertySourceClient
+)
+
+type CMPropertyConfig struct {
+	Type       reflect.Kind
+	Source     CMPropertySource
+	Optional   bool
+	SampleType CMPropertySampleType
+}
+
+func requiredPropertyConfig(t reflect.Kind, sampleType CMPropertySampleType) *CMPropertyConfig {
+	return &CMPropertyConfig{
+		Type:       t,
+		Source:     CMPropertySourceLib,
+		Optional:   false,
+		SampleType: sampleType,
+	}
+}
+func optionalPropertyConfig(t reflect.Kind, sampleType CMPropertySampleType) *CMPropertyConfig {
+	return &CMPropertyConfig{
+		Type:       t,
+		Source:     CMPropertySourceLib,
+		Optional:   true,
+		SampleType: sampleType,
+	}
+}
+func wellKnownPropertyConfig(t reflect.Kind, sampleType CMPropertySampleType) *CMPropertyConfig {
+	return &CMPropertyConfig{
+		Type:       t,
+		Source:     CMPropertySourceClient,
+		Optional:   true,
+		SampleType: sampleType,
+	}
+}
+
+// TODO: audit the sample types
+func BuiltInPropertyTypes() map[string]*CMPropertyConfig {
+	return map[string]*CMPropertyConfig{
+		"platform":                requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"os_version":              requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"device_manufacturer":     requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"device_model":            requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"device_model_class":      requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"locale_language_code":    requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"locale_country_code":     requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"locale_currency_code":    requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"app_version":             requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"user_interface_idiom":    requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"app_id":                  requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"screen_width_pixels":     requiredPropertyConfig(reflect.Int, CMPropertySampleTypeAppStart),
+		"screen_height_pixels":    requiredPropertyConfig(reflect.Int, CMPropertySampleTypeAppStart),
+		"screen_width_points":     requiredPropertyConfig(reflect.Int, CMPropertySampleTypeAppStart),
+		"screen_height_points":    requiredPropertyConfig(reflect.Int, CMPropertySampleTypeAppStart),
+		"screen_scale":            requiredPropertyConfig(reflect.Float64, CMPropertySampleTypeAppStart),
+		"device_battery_state":    requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"device_battery_level":    requiredPropertyConfig(reflect.Float64, CMPropertySampleTypeAppStart),
+		"device_low_power_mode":   requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"device_orientation":      requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"interface_orientation":   requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"dark_mode":               requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"network_connection_type": requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"has_wifi_connection":     requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"has_cell_connection":     requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"has_active_network":      requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"expensive_network":       requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"cm_version":              requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"foreground":              requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"app_install_date":        requiredPropertyConfig(CMTimeKind, CMPropertySampleTypeAppStart),
+		"timezone_gmt_offset":     requiredPropertyConfig(reflect.Int, CMPropertySampleTypeAppStart),
+		"app_state":               requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"has_watch":               requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"screen_brightness":       requiredPropertyConfig(reflect.Float64, CMPropertySampleTypeAppStart),
+		"screen_captured":         requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
 
 		// Audio
-		"other_audio_playing": reflect.Bool,
-		"has_headphones":      reflect.Bool,
-		"has_bt_headphones":   reflect.Bool,
-		"has_bt_headset":      reflect.Bool,
-		"has_wired_headset":   reflect.Bool,
-		"has_car_audio":       reflect.Bool,
-		"on_call":             reflect.Bool,
+		"other_audio_playing": requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"has_headphones":      requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"has_bt_headphones":   requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"has_bt_headset":      requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"has_wired_headset":   requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"has_car_audio":       requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"on_call":             requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
 
 		// Location
-		"location_permission":          reflect.Bool,
-		"location_permission_detailed": reflect.String,
-		"location_latitude":            reflect.Float64,
-		"location_longitude":           reflect.Float64,
-		"location_city":                reflect.String,
-		"location_region":              reflect.String,
-		"location_country":             reflect.String,
-		"location_approx_city":         reflect.String,
-		"location_approx_region":       reflect.String,
-		"location_approx_country":      reflect.String,
-		"location_approx_latitude":     reflect.Float64,
-		"location_approx_longitude":    reflect.Float64,
+		"location_permission":          requiredPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
+		"location_permission_detailed": requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"location_latitude":            requiredPropertyConfig(reflect.Float64, CMPropertySampleTypeAppStart),
+		"location_longitude":           requiredPropertyConfig(reflect.Float64, CMPropertySampleTypeAppStart),
+		"location_city":                requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"location_region":              requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"location_country":             requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"location_approx_city":         requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"location_approx_region":       requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"location_approx_country":      requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"location_approx_latitude":     requiredPropertyConfig(reflect.Float64, CMPropertySampleTypeAppStart),
+		"location_approx_longitude":    requiredPropertyConfig(reflect.Float64, CMPropertySampleTypeAppStart),
 
 		// Permisions
-		"notifications_permission": reflect.String,
-		"microphone_permission":    reflect.String,
-		"camera_permission":        reflect.String,
-		"contacts_permission":      reflect.String,
-		"photo_library_permission": reflect.String,
-		"add_photo_permission":     reflect.String,
-		"calendar_permission":      reflect.String,
-		"reminders_permission":     reflect.String,
-		"bluetooth_permission":     reflect.String,
+		"notifications_permission": requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"microphone_permission":    requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"camera_permission":        requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"contacts_permission":      requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"photo_library_permission": requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"add_photo_permission":     requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"calendar_permission":      requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"reminders_permission":     requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"bluetooth_permission":     requiredPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
 
-		// Optional built in props (listed below in IsBuiltInPropertyOptional)
-		"device_model_version": reflect.String,
-		"low_data_mode":        reflect.Bool,
-	}
-}
+		// Optional built in props
+		"device_model_version": optionalPropertyConfig(reflect.String, CMPropertySampleTypeAppStart),
+		"low_data_mode":        optionalPropertyConfig(reflect.Bool, CMPropertySampleTypeAppStart),
 
-func IsBuiltInPropertyOptional(key string) bool {
-	optionalBuiltInProps := map[string]bool{
-		"device_model_version": true,
-		"low_data_mode":        true,
-	}
-	_, ok := optionalBuiltInProps[key]
-	return ok
-}
-
-func WellKnownPropertyTypes() map[string]reflect.Kind {
-	return map[string]reflect.Kind{
-		"user_signup_date": CMTimeKind,
+		// Well known properties - client should provide
+		"user_signup_date": wellKnownPropertyConfig(CMTimeKind, CMPropertySampleTypeOnCustomSet),
 	}
 }
 
