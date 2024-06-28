388dbfe784c5ae3cb3be563ba9576eed5895794b
scosman <scosman@users.noreply.github.com>
2024-06-28 15:11:58 +0300

    Add local delivery window and days of week to notifications

--- go/cmcore/data_model/notification.go
+++ go/cmcore/data_model/notification.go
@@ -2,19 +2,43 @@ package datamodel
 
 import (
 	"encoding/json"
+	"strings"
+	"time"
 )
+
+var allDaysOfWeek = []time.Weekday{
+	time.Sunday,
+	time.Monday,
+	time.Tuesday,
+	time.Wednesday,
+	time.Thursday,
+	time.Friday,
+	time.Saturday,
+}
+
+// 9am to 9pm
+const defaultDeliveryWindowLocalTimeStart = 9 * 60 * 60
+const defaultDeliveryWindowLocalTimeEnd = 21 * 60 * 60
 
 type Notification struct {
 	ID         string
 	Title      string
 	Body       string
 	ActionName string
+
+	DeliveryDaysOfWeek                []time.Weekday
+	DeliveryWindowLocalTimeOfDayStart int
+	DeliveryWindowLocalTimeOfDayEnd   int
 }
 
 type jsonNotification struct {
 	Title      string `json:"title,omitempty"`
 	Body       string `json:"body,omitempty"`
 	ActionName string `json:"actionName,omitempty"`
+
+	DeliveryDaysOfWeekString          string `json:"deliveryDaysOfWeek,omitempty"`
+	DeliveryWindowLocalTimeOfDayStart *int   `json:"deliveryLocalTimeOfDayStart,omitempty"`
+	DeliveryWindowLocalTimeOfDayEnd   *int   `json:"deliveryLocalTimeOfDayEnd,omitempty"`
 }
 
 func (a *Notification) Validate() bool {
@@ -32,24 +56,66 @@ func (n *Notification) ValidateReturningUserReadableIssueIgnoreID(ignoreID bool)
 	if n.Title == "" {
 		return "Notifications must have a title."
 	}
+	if len(n.DeliveryDaysOfWeek) == 0 {
+		return "Notifications must have at least one day of week valid for delivery."
+	}
 	return ""
 }
 
-func (a *Notification) UnmarshalJSON(data []byte) error {
+func (n *Notification) UnmarshalJSON(data []byte) error {
 	var jn jsonNotification
 	err := json.Unmarshal(data, &jn)
 	if err != nil {
 		return NewUserPresentableErrorWSource("Unable to parse the json of an notification with type=notification. Check the format, variable names, and types (eg float vs int).", err)
 	}
 
-	a.Title = jn.Title
-	a.Body = jn.Body
-	a.ActionName = jn.ActionName
+	n.Title = jn.Title
+	n.Body = jn.Body
+	n.ActionName = jn.ActionName
+
+	if jn.DeliveryDaysOfWeekString != "" {
+		n.DeliveryDaysOfWeek = parseDaysOfWeekString(jn.DeliveryDaysOfWeekString)
+	} else {
+		// Default to all days of week
+		n.DeliveryDaysOfWeek = allDaysOfWeek
+	}
+	if jn.DeliveryWindowLocalTimeOfDayStart != nil {
+		n.DeliveryWindowLocalTimeOfDayStart = *jn.DeliveryWindowLocalTimeOfDayStart
+	} else {
+		n.DeliveryWindowLocalTimeOfDayStart = defaultDeliveryWindowLocalTimeStart
+	}
+	if jn.DeliveryWindowLocalTimeOfDayEnd != nil {
+		n.DeliveryWindowLocalTimeOfDayEnd = *jn.DeliveryWindowLocalTimeOfDayEnd
+	} else {
+		n.DeliveryWindowLocalTimeOfDayEnd = defaultDeliveryWindowLocalTimeEnd
+	}
 
 	// ignore ID which is set later from primary config
-	if validationIssue := a.ValidateReturningUserReadableIssueIgnoreID(true); validationIssue != "" {
+	if validationIssue := n.ValidateReturningUserReadableIssueIgnoreID(true); validationIssue != "" {
 		return NewUserPresentableError(validationIssue)
 	}
 
 	return nil
+}
+
+// Parsed comman separated day of week strings, removing dupes and standardizing order
+func parseDaysOfWeekString(i string) []time.Weekday {
+	days := []time.Weekday{}
+	components := strings.Split(i, ",")
+
+	for dow := range allDaysOfWeek {
+		dowWD := time.Weekday(dow)
+		dowName := dowWD.String()
+		dayFound := false
+		for _, dayStringComponent := range components {
+			if dowName == dayStringComponent {
+				dayFound = true
+			}
+		}
+		if dayFound {
+			days = append(days, dowWD)
+		}
+	}
+
+	return days
 }
--- go/cmcore/data_model/notification_test.go
+++ go/cmcore/data_model/notification_test.go
@@ -3,16 +3,19 @@ package datamodel
 import (
 	"encoding/json"
 	"os"
+	"slices"
 	"testing"
+	"time"
 )
 
 func TestNotificationActionValidators(t *testing.T) {
 	// valid
 	a := Notification{
-		Title:      "Title",
-		Body:       "Body",
-		ActionName: "ActionName",
-		ID:         "io.criticalmoments.test",
+		Title:              "Title",
+		Body:               "Body",
+		ActionName:         "ActionName",
+		ID:                 "io.criticalmoments.test",
+		DeliveryDaysOfWeek: []time.Weekday{time.Monday, time.Tuesday},
 	}
 	if !a.Validate() {
 		t.Fatal(a.ValidateReturningUserReadableIssue())
@@ -40,7 +43,7 @@ func TestNotificationActionValidators(t *testing.T) {
 }
 
 func TestJsonParsingMinimalFieldsNotif(t *testing.T) {
-	testFileData, err := os.ReadFile("./test/testdata/actions/notifications/valid/minimalValidAlert.json")
+	testFileData, err := os.ReadFile("./test/testdata/actions/notifications/valid/minimalValidNotif.json")
 	if err != nil {
 		t.Fatal()
 	}
@@ -62,10 +65,19 @@ func TestJsonParsingMinimalFieldsNotif(t *testing.T) {
 	if n.ID != "" {
 		t.Fatal("ID should be nil")
 	}
+	if !slices.Equal(n.DeliveryDaysOfWeek, allDaysOfWeek) {
+		t.Fatal("failed to parse default delivery days of week")
+	}
+	if n.DeliveryWindowLocalTimeOfDayStart != defaultDeliveryWindowLocalTimeStart {
+		t.Fatal("failed to parse default delivery start time")
+	}
+	if n.DeliveryWindowLocalTimeOfDayEnd != defaultDeliveryWindowLocalTimeEnd {
+		t.Fatal("failed to parse default delivery end time")
+	}
 }
 
 func TestJsonParsingMaxFieldsNotif(t *testing.T) {
-	testFileData, err := os.ReadFile("./test/testdata/actions/notifications/valid/maximalValidAlert.json")
+	testFileData, err := os.ReadFile("./test/testdata/actions/notifications/valid/maximalValidNotif.json")
 	if err != nil {
 		t.Fatal()
 	}
@@ -84,6 +96,15 @@ func TestJsonParsingMaxFieldsNotif(t *testing.T) {
 	if n.ActionName != "actionName" {
 		t.Fatal("failed to parse actionName")
 	}
+	if !slices.Equal(n.DeliveryDaysOfWeek, []time.Weekday{time.Monday, time.Tuesday}) {
+		t.Fatal("failed to parse delivery days of week")
+	}
+	if n.DeliveryWindowLocalTimeOfDayStart != 60 {
+		t.Fatal("failed to parse delivery start time")
+	}
+	if n.DeliveryWindowLocalTimeOfDayEnd != 120 {
+		t.Fatal("failed to parse delivery end time")
+	}
 }
 
 func TestJsonParsingInvalidNotif(t *testing.T) {
@@ -95,5 +116,30 @@ func TestJsonParsingInvalidNotif(t *testing.T) {
 	err = json.Unmarshal(testFileData, &n)
 	if err == nil {
 		t.Fatal("Allowed invalid json")
+	}
+}
+
+func TestParseDowString(t *testing.T) {
+	cases := map[string][]time.Weekday{
+		"Monday,Tuesday": {time.Monday, time.Tuesday},
+		// dupes
+		"Monday,Monday,Tuesday": {time.Monday, time.Tuesday}, // add_test_case
+		// Invalid data
+		"Monday,Febuary,Tuesday": {time.Monday, time.Tuesday}, // add_test_case
+		// robustness
+		"Monday,,Tuesday": {time.Monday, time.Tuesday}, // add_test_case
+		// fix order
+		"Monday,Tuesday,Sunday": {time.Sunday, time.Monday, time.Tuesday}, // add_test_case
+		// empty
+		"": {}, // add_test_case
+		// all
+		"Sunday,Monday,Tuesday,Wednesday,Thursday,Friday,Saturday": {time.Sunday, time.Monday, time.Tuesday, time.Wednesday, time.Thursday, time.Friday, time.Saturday}, // add_test_case
+	}
+
+	for dowString, target := range cases {
+		dowResult := parseDaysOfWeekString(dowString)
+		if !slices.Equal(dowResult, target) {
+			t.Fatalf("Day of week string failed parsing for: %v", dowResult)
+		}
 	}
 }
