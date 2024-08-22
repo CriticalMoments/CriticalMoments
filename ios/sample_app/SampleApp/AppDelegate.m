6c2e6a1729bd5954ad057f7d23ec67bee6a92847
scosman <scosman@users.noreply.github.com>
2024-08-22 16:02:52 -0400

    Move config to GH pages in same repo

--- ios/sample_app/SampleApp/AppDelegate.m
+++ ios/sample_app/SampleApp/AppDelegate.m
@@ -37,8 +37,8 @@
 #endif
 
     [CriticalMoments.sharedInstance setDevelopmentConfigName:@"config.json"];
-    NSString *webBasedConfigUrl =
-        @"https://storage.googleapis.com/critical-moments-test-cases/sampleAppTestConfig.cmconfig";
+    // Deployed via github pages. Manually build using sign_sample_app_config.sh, then merge that to the docs branch
+    NSString *webBasedConfigUrl = @"https://criticalmoments.github.io/CriticalMoments/sample_app_config.cmconfig";
     [CriticalMoments.sharedInstance setReleaseConfigUrl:webBasedConfigUrl];
     [CriticalMoments.sharedInstance start];
 
---
+++ sign_sample_app_config.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+JSON_CONFIG='./ios/sample_app/SampleApp/config.json'
+SIGNED_NAME='./docs/sample_app_config.cmconfig'
+
+echo "Signing config file..."
+mkdir -p docs 
+status_code=$(curl -s -X POST --data-binary @"${JSON_CONFIG}" -w "%{response_code}" --header "Content-Type: application/json" https://criticalmoments.io/account/api/sign_config -o "${SIGNED_NAME}") 
+echo "Config signed with status code: $status_code"
+if [ $status_code != "200" ]; then 
+  echo "Error signing config: $status_code"
+  cat ${SIGNED_NAME}
+  rm ${SIGNED_NAME}
+  exit 1
+else
+  echo "Config signed successfully: ${SIGNED_NAME}"
+fi
