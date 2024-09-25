729ecf3bff38ddefc2959120a1b67752dd78b555
scosman <scosman@users.noreply.github.com>
2024-09-25 13:42:53 -0400

    New "starter" config

--- ios/sample_app/SampleApp/AppDelegate.m
+++ ios/sample_app/SampleApp/AppDelegate.m
@@ -36,7 +36,8 @@
     [CriticalMoments.sharedInstance setLogEvents:true];
 #endif
 
-    [CriticalMoments.sharedInstance setDevelopmentConfigName:@"config.json"];
+    [CriticalMoments.sharedInstance setDevelopmentConfigName:@"cmDevConfig.json"];
+    //[CriticalMoments.sharedInstance setDevelopmentConfigName:@"starterConfig.json"];
     // Deployed via github pages. Manually build using sign_sample_app_config.sh, then merge that to the docs branch
     NSString *webBasedConfigUrl = @"https://criticalmoments.github.io/CriticalMoments/sample_app_config.cmconfig";
     [CriticalMoments.sharedInstance setReleaseConfigUrl:webBasedConfigUrl];
